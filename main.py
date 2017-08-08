eb9cae8811abe69ce800089a9dc13a7a5d1dc967
JinhaoLei <leijh14@gamil.com>
2017-08-08 14:48:20 -0700

    modified:   main.py 	modified:   preprocess.py

--- main.py
+++ main.py
@@ -1,70 +1,56 @@
 import numpy
 import tensorflow as tf
 import pickle
-import config
 import time
 import random
 import preprocess
 import util
-
+import os
+from score import start
 from tensorflow.contrib.rnn.python.ops.core_rnn_cell import GRUCell, BasicLSTMCell, MultiRNNCell
 from tensorflow.contrib.rnn import DropoutWrapper
-
-tf.app.flags.DEFINE_float("init_scale", 0.05, "initialize range")
-tf.app.flags.DEFINE_string("sent_pkl_file", "sen_pkl_file.csv", "sentence file")
-tf.app.flags.DEFINE_string("word_vec_pkl_file", "word_vector.csv", "word embedding file")
-tf.app.flags.DEFINE_float("lr", 1.0, "learning rate")
-tf.app.flags.DEFINE_float("keep_prob", 0.5, "keep probability")
-tf.app.flags.DEFINE_integer("batch_size", 128, "batch_size")
-tf.app.flags.DEFINE_integer("max_max_epoch", 30, "iteration")
-tf.app.flags.DEFINE_integer("hidden_size", 50, "hidden size")
-tf.app.flags.DEFINE_float("max_grad_norm", 5.0, "max grad")
-tf.app.flags.DEFINE_boolean("max_pooling", True, "max pooling or mean pooling")
-tf.app.flags.DEFINE_integer("mslen", , "uniform sentence length")
-
-FLAGS = tf.app.flags.FLAGS
-
 class PTBModel(object):
-    def __init__(self, is_training, use_dropout, max_pooling):
+    def __init__(self, is_training, use_dropout, max_pooling, batch_size):
         with tf.variable_scope('main'):
-            self.x = tf.placeholder(name='x', dtype=tf.int32, shape=(batch_size, mslen))
+            self.x = tf.placeholder(name='x', dtype=tf.int32, shape=(batch_size, FLAGS.mslen))
             self.y = tf.placeholder(name='y', dtype=tf.int32, shape=(batch_size, ))
             self.length = tf.placeholder(name='length', dtype=tf.int64, shape=(batch_size))
-            pretrained_embedding = pickle.load(open(word_vec_pkl_file))
+            pretrained_embedding = pickle.load(open(FLAGS.word_vec_pkl_file))
             pretrained_embedding = numpy.array(pretrained_embedding, dtype='float32')
-            self.embedding = tf.get_variable(name='embedding',
-                            dtype=tf.float32, initializer=pretrained_embedding, trainable=True)
+            print(numpy.shape(pretrained_embedding))
+            self.embedding = tf.get_variable(name='embedding',  dtype=tf.float32, initializer=pretrained_embedding, trainable=True)
             inputs = tf.nn.embedding_lookup(self.embedding, self.x)
             print('embedding finished')
             if use_dropout:
-                inputs = tf.nn.dropout(inputs, keep_prob)
-            flstm_cell = BasicLSTMCell(hidden_size, forget_bias=0.0, state_is_tuple=True)
-            blstm_cell = BasicLSTMCell(hidden_size, forget_bias=0.0, state_is_tuple=True)
+                inputs = tf.nn.dropout(inputs, FLAGS.keep_prob)
+            flstm_cell = BasicLSTMCell(FLAGS.hidden_size, forget_bias=0.0, state_is_tuple=True)
+            blstm_cell = BasicLSTMCell(FLAGS.hidden_size, forget_bias=0.0, state_is_tuple=True)
                 
             if use_dropout:
-                flstm_cell = DropoutWrapper(flstm_cell, output_keep_prob=keep_prob)
-                blstm_cell = DropoutWrapper(blstm_cell, output_keep_prob=keep_prob)
+                flstm_cell = DropoutWrapper(flstm_cell, output_keep_prob=FLAGS.keep_prob)
+                blstm_cell = DropoutWrapper(blstm_cell, output_keep_prob=FLAGS.keep_prob)
                 
-            self._initial_state_f = flstm_cell.zero_state(batch_size, tf.float32)
-            self._initial_state_b = blstm_cell.zero_state(batch_size, tf.float32)
+            self._initial_state_f = flstm_cell.zero_state(FLAGS.batch_size, tf.float32)
+            self._initial_state_b = blstm_cell.zero_state(FLAGS.batch_size, tf.float32)
                 
             outputs, _ = tf.nn.bidirectional_dynamic_rnn(
                     flstm_cell, blstm_cell, inputs, sequence_length=self.length,
                 dtype=tf.float32, initial_state_fw =flstm_cell.zero_state(batch_size, tf.float32), initial_state_bw = blstm_cell.zero_state(batch_size, tf.float32))
-            outputs = tf.concat(outputs, 2) #batach mslen hidden
+            outputs = tf.concat(outputs, 2) #batach FLAGS.mslen hidden
             output = tf.reduce_max(outputs, 1)#batch hidden
               
             print('bi-LSTM finished')
 
                
 
-            w = tf.get_variable('w', [2 * hidden_size, 53], tf.float32)
-            b = tf.get_variable('b', [1, 53], tf.float32)
+            w = tf.get_variable('w', [2 * FLAGS.hidden_size, 42], tf.float32)
+            b = tf.get_variable('b', [1, 42], tf.float32)
             tmatrix = tf.tanh(tf.matmul(output, w) + b)
                 
 
             pre_dis = tf.nn.softmax(tmatrix)
-            self.pre_label = tf.reduce_max(pre_dis, 1)
+            #print(pre_dis)
+            self.pre_label = tf.argmax(pre_dis, -1)
             #y_mask = tf.one_hot(self.y, 53)
                 
             loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=pre_dis, labels=self.y)
@@ -80,9 +66,9 @@ class PTBModel(object):
       
             tvars = tf.trainable_variables()
     
-            grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars), max_grad_norm)
+            grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars), FLAGS.max_grad_norm)
     
-            optimizer = tf.train.GradientDescentOptimizer(self._lr)
+            optimizer = tf.train.AdagradOptimizer(self._lr)
         
             self._train_op = optimizer.apply_gradients(zip(grads, tvars))
         
@@ -99,21 +85,29 @@ class PTBModel(object):
     def lr(self):       
         return self._lr
 
-def sta(pre_label, gold_label):
-    equal = 0
-    for i in range(len(gold_label)):
-        if gold_label[i] == 0:
-            continue
-        else:
-            if gold_label[i] == pre_label[i]:
-                equal += 1
-    return equal
+def sta(pre_label,gold_label):
+    dic = {}
+    with open('re_map.csv') as f:
+        for n, line in enumerate(f):
+            s = line.split('\t')
+            dic[s[1].strip()] = s[0]
+    f = open('pre.csv', 'w')
+    for i in pre_label:
+        f.write(dic[str(i)] + '\n')
+    f.close()
+    f = open('gold.csv', 'w')
+    for i in gold_label:
+        for j in i:
+            f.write(dic[str(j)] + '\n')
+    f.close()
+    return start()
 
 def run_epoch(session, model, data, label, length, test=False):
     if test == True:
         batch_size = 1
+    else:
+        batch_size = FLAGS.batch_size
     epoch_size = (len(data) // batch_size)
-
     statef = session.run(model._initial_state_f)
     stateb = session.run(model._initial_state_b)
 
@@ -121,9 +115,9 @@ def run_epoch(session, model, data, label, length, test=False):
     total = len(data)
     label = label[:len(label)-len(label)%batch_size]
     length = length[:len(length)-len(length)%batch_size]
-    total = 0
     total_loss = 0.0
     total_correct = 0
+    pre = []
     sids = list(range(len(data)))
     if test == False:
         random.shuffle(sids)
@@ -159,22 +153,26 @@ def run_epoch(session, model, data, label, length, test=False):
         feed_dict[model.y] = alabel[ii]
         feed_dict[model.length] = final_length[ii]
         if test == False:
+            #fetches = [model.loss, model.pre_label]
             fetches = [model.loss, model.pre_label, model._train_op]
             loss, pre_label, _ =session.run(fetches, feed_dict) 
+            #loss, pre_label = session.run(fetches, feed_dict)
             total_loss += loss
-            total_correct += sta(pre_label, alabel[ii])
+            pre.extend(pre_label)
+            #total_correct += sta(pre_label, alabel[ii])
         else:
-            fetches = [model.loss, model.equal]
-            loss, equal, _ =session.run(fetches, feed_dict) 
+            fetches = [model.loss, model.pre_label]
+            loss, pre_label =session.run(fetches, feed_dict) 
             total_loss += loss
-            total_correct += sta(pre_label, alabel[ii])
-
+            pre.extend(pre_label)
+            #total_correct += sta(pre_label, alabel[ii])
+    #print(numpy.shape(alabel))
+    p, r, f1 = sta(pre, alabel)
            
-    P = total_correct / total
     loss = total_loss / float(epoch_size)
-    print("P: %d/%d %.3f"%(total_correct, total, P))
+    print("P: %.3f R: %.3f F1: %.3f"%(p, r, f1))
     print("loss:%.7f "%(loss))
-    return 0
+    return f1
 
 
 if __name__=='__main__':
@@ -182,23 +180,37 @@ if __name__=='__main__':
     #preprocess.build_vocab()
     #preprocess.convert_to_id()
     #preprocess.prepare_word_vec()
-    sentences = pickle.load(open(sent_pkl_file))
+    tf.app.flags.DEFINE_float("init_scale", 0.05, "initialize range")
+    tf.app.flags.DEFINE_string("sent_pkl_file", "sen_pkl_file.csv", "sentence file")
+    tf.app.flags.DEFINE_string("word_vec_pkl_file", "word_vector.csv", "word embedding file")
+    tf.app.flags.DEFINE_float("lr", 1.0, "learning rate")
+    tf.app.flags.DEFINE_float("lr_decay", 0.9, "lr decay")
+    tf.app.flags.DEFINE_float("keep_prob", 0.5, "keep probability")
+    tf.app.flags.DEFINE_integer("batch_size", 50, "batch_size")
+    tf.app.flags.DEFINE_integer("max_max_epoch", 30, "iteration")
+    tf.app.flags.DEFINE_integer("max_epoch", 20, "iteration")
+    tf.app.flags.DEFINE_integer("hidden_size", 50, "hidden size")
+    tf.app.flags.DEFINE_float("max_grad_norm", 5.0, "max grad")
+    tf.app.flags.DEFINE_boolean("max_pooling", True, "max pooling or mean pooling")
+    tf.app.flags.DEFINE_integer("mslen", 96, "uniform sentence length")
+
+    FLAGS = tf.app.flags.FLAGS
+
+    sentences = pickle.load(open(FLAGS.sent_pkl_file))
+    #print(sentences[0])
     _config = tf.ConfigProto()
     _config.gpu_options.allow_growth = True 
-    train_data_1, train_data_2, train_data_3, train_label_1, train_label_2, train_label_3, 
-    train_len_1, train_len_2, train_len_3, valid_data_1, valid_data_2, valid_data_3, 
-    valid_label_1, valid_label_2, valid_label_3, valid_len_1, valid_len_2, valid_len_3,
-    test_data, test_label, test_len= sentences
-    for cross_label in range(1, 4):
+    train_data_1, train_label_1, train_len_1, valid_data_1, valid_label_1, valid_len_1, test_data, test_label, test_len = sentences
+    for cross_label in range(1, 2):
         with tf.Graph().as_default(), tf.Session(config=_config) as session:
-            initializer = tf.random_uniform_initializer(-init_scale, init_scale)
+            initializer = tf.random_uniform_initializer(-FLAGS.init_scale, FLAGS.init_scale)
             #saver_path = saver.save(session, "/home/leijh/model1/extract.ckpt")
             with tf.variable_scope("model", reuse=None, initializer=initializer):
-                m = PTBModel(is_training=True, use_dropout=True, max_pooling = max_pooling)   
+                m = PTBModel(is_training=True, use_dropout=True, max_pooling = FLAGS.max_pooling, batch_size = FLAGS.batch_size)   
             with tf.variable_scope("model", reuse=True, initializer=initializer):
-                mvalid = PTBModel(is_training=False, use_dropout=False, max_pooling = max_pooling)
+                mvalid = PTBModel(is_training=False, use_dropout=False, max_pooling = FLAGS.max_pooling, batch_size = 1)
             with tf.variable_scope("model", reuse=True, initializer=initializer):
-                mtest = PTBModel(is_training=False, use_dropout=False, max_pooling = max_pooling)
+                mtest = PTBModel(is_training=False, use_dropout=False, max_pooling = FLAGS.max_pooling, batch_size = 1)
         #saver=tf.train.Saver()
         #ckpt = tf.train.get_checkpoint_state("/home/leijh/model1/exp66")        
         #saver.restore(session,ckpt.model_checkpoint_path)
@@ -211,23 +223,35 @@ if __name__=='__main__':
                 valid_data = valid_data_1
                 train_len = train_len_1
                 valid_len = valid_len_1
+                train_label = train_label_1
+                valid_label = valid_label_1
             elif cross_label == 2:
                 train_data = train_data_2
                 valid_data = valid_data_2
                 train_len = train_len_2
                 valid_len = valid_len_2
+                train_label = train_label_2
+                valid_label = valid_label_2
             else:
                 train_data = train_data_3
                 valid_data = valid_data_3
                 train_len = train_len_3
                 valid_len = valid_len_3
+                train_label = train_label_3
+                valid_label = valid_label_3
             maxvalid = -1.0
-            for i in range(max_max_epoch):   #
-            # learning rate
+            #print(len(train_data))
+            for i in range(FLAGS.max_max_epoch):   #
+                if i>=20:
+                    if valid_per<=maxvalid:
+                        lr_decay = FLAGS.lr_decay ** max(i - FLAGS.max_epoch, 0.0) 
+                        m.assign_lr(session, FLAGS.lr * FLAGS.lr_decay)
+                else:
+                    m.assign_lr(session, FLAGS.lr)      
             #  llr_decay = 0.5^(i-max_epoch)
                 #lr_decay = config.lr_decay ** max(i - config.max_epoch, 0.0)
                 #m.assign_lr(session, config.learning_rate * lr_decay) # learning rate
-                m.assign_lr(session, lr)
+                #m.assign_lr(session, FLAGS.lr)
                 print("Epoch: %d Learning rate: %.5f" % (i + 1, session.run(m.lr)))
                 print("train-%d:"%(cross_label))
                 train_per = run_epoch(session, m, train_data, train_label, train_len)
@@ -236,7 +260,7 @@ if __name__=='__main__':
                 valid_per = run_epoch(session, mvalid, valid_data, valid_label, valid_len, test=True)
             #print("Epoch: %d Valid Perplexity: %.3f" % (i + 1, valid_perplexity))
             
-                #if valid_per > maxvalid:
+                
                 #    maxvalid = valid_per
                 #    print("test:")
-                #    test_result = run_epoch(session, mtest, test_data, test_lable_s, test_lable_e, test_len, test_gold, test = True)
+                #    test_result = run_epoch(session, mtest, test_data, test_lable_s, test_lable_e, test_len, test_g, test = True)
--- preprocess.py
+++ preprocess.py
@@ -8,57 +8,82 @@ import collections
 import pickle
 import numpy
 import fileinput
+import argparse
+#import config
 
-import config
-
-import util
+#import util
 
 
 
+def load_word(args):
+    words = [line.split()[0] for line in open(args.vocab_file)]
+    return words
+
+
+def load_word_to_id(args):
+    words = load_word(args)
+    word_to_id = dict(zip(words, range(len(words))))
+    return word_to_id
 
 #@util.file_checker(config.vocab_file)
-def build_vocab():
+def build_vocab(args):
     counter = collections.Counter()
-    f = fileinput.input(files=dataset_file)
+    f = fileinput.input(files=args.dataset_file)
     linenumber=0
     maxlen = 0
-    for line in f:
-        words = line.split('\t')[5].split()
+    max_index = -1
+    for n, line in enumerate(f): 
+        if n % 10000 ==0:
+            print("read vocab %d"%(n))
+        words = line.strip().split()
         counter.update(words)
-        if len(words) > maxlen:
-            maxlen = len(words)
+        if len(words) >maxlen:
+            maxlen = len(words)       
         linenumber+=1    
     f.close()
     print(maxlen)
-    with open(vocab_file, 'w') as vocab_file:
+    #print()
+    with open(args.vocab_file, 'w') as vocab_file:
         for w, n in counter.most_common():
-            vocab_file.write('%s %d\n' % (w, n))
+            if n>=2:
+                vocab_file.write('%s %d\n' % (w, n))
 
-def relation_2id():
+def relation_2id(args):
     relation_map = {}
-    with open(relation2id_file) as f:
+    with open(args.relation2id_file) as f:
         for n, line in enumerate(f):
             s = line.split()
+            #print(s[0])
             relation_map[s[0]] = s[1].strip()
-
+    return relation_map
 
 #@util.file_checker(config.lm_sent_pkl_file)
-def convert_to_id():
-    word_to_id = util.load_word_to_id()
+def convert_to_id(args, relation_map):
+    word_to_id = load_word_to_id(args)
     unk_id = 2286
-    sents = []
-    labels=[]
-    lengths=[]
-    with open(train_data_file) as lm_sent_txt_file:
-        for n, line in enumerate(lm_sent_txt_file):
-            ids = [word_to_id.get(w, unk_id) for w in line.split('\t')[5].split()]
-            sents.append(ids)
-            label = relation_map[line.split('\t')[4]]
-            labels.append(label)
-            length = len(ids)
-            lengths.append(length)
+    def convert(type, data_file, gold_file):
+        sents = []
+        labels=[]
+        lengths=[]
+        with open(data_file) as lm_sent_txt_file:
+            for n, line in enumerate(lm_sent_txt_file):
+                if n % 10000 ==0:
+                    print ('read_%s_data%d'%(type, n))
+                ids = [word_to_id.get(w, unk_id) for w in line.strip().split()]
+                ids.extend([unk_id] * (args.max_len - len(ids)))
+                sents.append(ids)
+                length = len(ids)
+                lengths.append(length)
+        with open(gold_file) as gold_file:
+            for n, line in enumerate(gold_file):
+                if n % 10000 ==0:
+                    print ('read_%s_gold%d'%(type, n))
+                label = relation_map[line.strip()]
+                labels.append(label)
+        return sents, labels, lengths
+     
     
-    sids = list(range(len(sents)))
+    '''sids = list(range(len(sents)))
     random.shuffle(sids)
     three = len(sents) / 3
     train_data_1 = [sents[si] for si in sids[:2 * three]]
@@ -84,68 +109,60 @@ def convert_to_id():
     
     valid_data_3 = [sents[si] for si in sids[three:2 * three]]
     valid_label_3=[labels[si] for si in sids[three:2 * three]]
-    valid_len_3=[lengths[si] for si in sids[three:2 * three]]
+    valid_len_3=[lengths[si] for si in sids[three:2 * three]]'''
 
-    sents = []
-    labels=[]
-    lengths=[]
 
-    with open(test_data_file) as lm_sent_txt_file:
-        for n, line in enumerate(lm_sent_txt_file):
-            ids = [word_to_id.get(w, unk_id) for w in line.split('\t')[5].split()]
-            sents.append(ids)
-            label = relation_map[line.split('\t')[4]]
-            labels.append(label)
-            length = len(ids)
-            lengths.append(length)
-    
-    sids = list(range(len(sents)))
-    test_data= [sents[si] for si in sids]
-    test_label=[labels[si] for si in sids]
-    test_len=[lengths[si] for si in sids]
-    
-    #for i in range(10):
-        #print(test[i],testlen[i],testgold[i],testcategory[i],testgold[i])
-    with open(lm_sent_pkl_file, 'wb') as lm_sent_pkl_file:
-        pickle.dump((train_data_1, train_data_2, train_data_3, train_label_1, train_label_2, train_label_3, 
-    train_len_1, train_len_2, train_len_3, valid_data_1, valid_data_2, valid_data_3, 
-    valid_label_1, valid_label_2, valid_label_3, valid_len_1, valid_len_2, valid_len_3,
-    test_data, test_label, test_len, testle,trainlen, validlen, testlen,traingold,validgold, testgold), lm_sent_pkl_file, pickle.HIGHEST_PROTOCOL)
+    train_data, train_label, train_len = convert('train', args.train_data_file, args.train_gold)
+    valid_data, valid_label, valid_len = convert('dev', args.dev_data_file, args.dev_gold)
+    test_data, test_label, test_len = convert('test', args.test_data_file, args.test_gold)
+
+    print(len(train_data), len(train_label), len(train_len))
+    #for i in range(3):
+    #    print(test[i],testlen[i],testgold[i],testcategory[i],testgold[i])
+    with open(args.lm_sent_pkl_file, 'wb') as lm_sent_pkl_file:
+        pickle.dump((train_data, train_label, train_len, valid_data, 
+            valid_label, valid_len, test_data, test_label, test_len), lm_sent_pkl_file, pickle.HIGHEST_PROTOCOL)
 
 #@util.file_checker(config.word_vec_pkl_file)
-def prepare_word_vec():
-    word_to_id = util.load_word_to_id()
-    vec = numpy.random.randn(len(word_to_id), word_vec_size)
-    with open(word_vec_txt_file) as vec_file:
+def prepare_word_vec(args):
+    word_to_id = load_word_to_id(args)
+    vec = numpy.random.randn(len(word_to_id), args.word_vec_size)
+    with open(args.word_vec_txt_file) as vec_file:
         for line in vec_file:
             its = line.split()
-            if len(its) != word_vec_size + 1:
+            if len(its) != args.word_vec_size + 1:
                 continue
             w = its[0]
             v = [float(t) for t in its[1:]]
             if w in word_to_id:
                 vec[word_to_id[w]] = v
     #print(word_to_id)
-    with open(word_vec_pkl_file, 'wb') as pkl_file:
+    with open(args.word_vec_pkl_file, 'wb') as pkl_file:
         pickle.dump(vec, pkl_file, pickle.HIGHEST_PROTOCOL)
 
 def main():
   #  prepare_sentence()
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--word_size', dest='word_vec_size', default=50)
-    parser.add_argument('--test_file', dest='test_data_file', default='test.txt')
-    parser.add_argument('--train_file', dest='train_data_file', default='train.txt')
+    parser.add_argument('--test_file', dest='test_data_file', default='test_data.csv')
+    parser.add_argument('--train_file', dest='train_data_file', default='train_data.csv')
+    parser.add_argument('--dev_file', dest='dev_data_file', default='dev_data.csv')
+    parser.add_argument('--test_gold', dest='test_gold', default='test.gold')
+    parser.add_argument('--train_gold', dest='train_gold', default='train.gold')
+    parser.add_argument('--dev_gold', dest='dev_gold', default='dev.gold')
     parser.add_argument('--word_vec_out_file', dest='word_vec_pkl_file', default='word_vector.csv')
-    parser.add_argument('--word_vec_txt', dest='word_vec_txt_file', default='nyt50.txt')
-    parser.add_argument('--relation2id_file', dest='relation2id_file', default='relation2id.csv')
+    parser.add_argument('--word_vec_txt', dest='word_vec_txt_file', default='/scr/leijh/data/glove.6B.50d.txt')
+    parser.add_argument('--relation_to_id_file', dest='relation2id_file', default="re_map.csv")
     parser.add_argument('--vocab_file', dest='vocab_file', default='vocabulary.csv')
     parser.add_argument('--all_file', dest='dataset_file', default='all.csv')
     parser.add_argument('--sen_pkl_file', dest='lm_sent_pkl_file', default='sen_pkl_file.csv')
+    parser.add_argument('--max_len', dest='max_len', default=96)
     args = parser.parse_args()
-    relation_2id()
-    build_vocab()
-    convert_to_id()
-    prepare_word_vec()
+    #relation_map = {}
+    relation_map = relation_2id(args)
+    build_vocab(args)
+    convert_to_id(args, relation_map)
+    prepare_word_vec(args)
 
 
 if __name__ == '__main__':
